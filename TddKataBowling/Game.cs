dee23763bbfaca9bc61456330a026a7dcec24f02
a-dumbadze-dodo <a.dumbadze@dodopizza.com>
2021-08-05 16:55:21 +0300

    add test for Strike

--- TddKataBowling/Game.cs
+++ TddKataBowling/Game.cs
@@ -1,18 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-namespace TddKataBowling {
-    public class Game {
+﻿using System.Collections.Generic;
 
-        private List<int> _score { get; set; }
-        public Game() {
-            this._score = new List<int>();
-        }
-        public void Roll(int knockedPins) {
-            this._score.Add(knockedPins);
-        }
-        public int GetScore() {
-            return this._score.Sum();
-        }
-    }
+namespace TddKataBowling
+{
+	public class Game
+	{
+		private List<int> _score { get; set; }
+
+		public Game()
+		{
+			this._score = new List<int>();
+		}
+
+		public void Roll(int knockedPins)
+		{
+			this._score.Add(knockedPins);
+		}
+
+		public int GetScore()
+		{
+			var result = 0;
+			for (int i = 0; i < _score.Count; i++)
+			{
+				var currentKnockedPins = _score[i];
+				var previousKnockedPins = i < 1 ? 0 : _score[i - 1];
+				var previous2KnockedPins = i < 2 ? 0 : _score[i - 2];
+
+				if (previousKnockedPins == 10 || previous2KnockedPins == 10)
+				{
+					result += currentKnockedPins * 2;
+				}
+				else
+				{
+					result += currentKnockedPins;
+				}
+			}
+			
+			return result;
+		}
+	}
 }
--- TddKataBowlingTests/UnitTest1.cs
+++ TddKataBowlingTests/UnitTest1.cs
@@ -13,16 +13,26 @@ namespace TddKataBowlingTests
 		}
 
 		[Test]
-		public void TestScore() {
-			
+		public void WhenRoll1_ThenScore1() {
 			var knockedPins = 1;
 			var game = new Game();
 			
 			game.Roll(knockedPins);
-			
 			var score = game.GetScore();
 			
-			Assert.AreEqual(score,knockedPins);
+			Assert.AreEqual(knockedPins, score);
+		}
+		
+		[Test]
+		public void WhenRollStrikeAnd5And3_ThenScore26() {
+			var game = new Game();
+			
+			game.Roll(10); // 10
+			game.Roll(5);  // 10
+			game.Roll(3);  // 6
+			var score = game.GetScore();
+			
+			Assert.AreEqual(26,score);
 		}
 	}
 }
